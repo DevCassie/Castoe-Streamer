cb6eae0370db8cc0fe70de7afffc26454ff42905
DevCassie <19227530+DevCassie@users.noreply.github.com>
2021-01-03 19:41:07 +0100

    Minor changes

--- src/Transports/index.js
+++ src/Transports/index.js
@@ -1,9 +1,38 @@
-// const CastoeConsole = require('./ConsoleTransport.js');
-// const CastoeStream = require('./StreamTransport.js');
-// const CastoeFile = require('./FileTransport.js');
+const CastoeConsole = require('./ConsoleTransport.js');
+const CastoeStream = require('./StreamTransport.js');
 
-module.exports = {
-	CastoeFile: require('./FileTransport.js'),
-	CastoeStream: require('./StreamTransport.js'),
-	CastoeConsole: require('./ConsoleTransport.js'),
-}
+// module.exports = {
+// 	CastoeFile: require('./FileTransport.js'),
+// 	CastoeStream: require('./StreamTransport.js'),
+// 	CastoeConsole: require('./ConsoleTransport.js'),
+// }
+
+/**
+ * @type {CastoeConsole}
+ */
+Object.defineProperty(exports, 'CastoeConsole', {
+	configurable: true,
+	enumerable: true,
+	get() {
+		return require('./ConsoleTransport.js');
+	}	
+});
+
+/**
+ * @type {CastoeStream}
+ */
+Object.defineProperty(exports, 'CastoeStream', {
+	configurable: true,
+	enumerable: true,
+	get() {
+		return require('./StreamTransport.js');
+	}
+});
+
+Object.defineProperty(exports, 'CastoeFile', {
+	configurable: true,
+	enumerable: true,
+	get() {
+		return require('./FileTransport.js');
+	}
+});
