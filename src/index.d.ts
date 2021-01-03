37051f666b1ce5155c9ae8043172d697ea70e8a7
DevCassie <19227530+DevCassie@users.noreply.github.com>
2021-01-03 22:06:55 +0100

    Minor updates

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,38 +1,6 @@
-import * as NodeJSStream from 'stream';
-
-declare namespace CastoeStreamer {
-  interface CastoeConsoleOptions {
-    name: String,
-    date: String,
-    traceFile: Boolean,
-    showType: Boolean,
-    colors: CastoeConsoleColorOptions
-  }
-  
-  interface CastoeConsoleColorOptions {
-    bigint: String,
-    boolean: String,
-    function: String,
-    number: String,
-    object: String,
-    string: String,
-    symbol: String
-  }
-  
-  interface sendMethod {
-    (info: any, callback: any): Logger;
-  }
-  
-  interface Logger extends NodeJSStream.Transform {
-    send: sendMethod;
-    clear(): Logger;
-    delete(): Logger;
-  }
-  
+declare namespace CastoeStreamer { 
   let version: string;
-  let clear: () => Logger;
-  let stream: (options?: any) => NodeJS.ReadableStream;
-  let send: sendMethod;
+  let author: string;
 }
 
 exports = CastoeStreamer;
