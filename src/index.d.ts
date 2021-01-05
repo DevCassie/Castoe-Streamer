6d59910aa5c1fe891b68ccd8f3bcdd9638c7a643
DevCassie <19227530+DevCassie@users.noreply.github.com>
2021-01-05 21:17:33 +0100

    Minor changes

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,11 @@
 import * as CastoeTransports from './Transports/index';
 
-export declare namespace castoeStreamer {
+declare namespace castoeStreamer {
   let Transports: CastoeTransports;
   
   let version: string;
   let author: string;
   let Transports: transports;
 }
+
+export = CastoeTransports;
